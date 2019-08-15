670b5891808e6c4b04a1fb43dcb89911f2066230
Andre Emidio <andresjc2008@gmail.com>
2019-08-15 15:36:57 -0300

    Atualização

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from '../assets/logo.svg';
 
+import like from '../assets/like.svg';
+import dislike from '../assets/dislike.svg';
+
 export default function Main({ match}){
 
     return (
         <div className = "main-container">
-            <img src = {logo} alt="Devs"/>
+            <img src = {logo} alt = "Dev" />  
             <ul>
                 <li>
                        <img src = "https://avatars0.githubusercontent.com/u/2787467?v=4" alt = "Avatar" /> 
@@ -13,6 +16,10 @@ export default function Main({ match}){
                            <strong>And</strong>
                            <p>SSSS</p>
                         </footer>
+                        <div className = "buttons">
+                            <button type ="button"><img src = {dislike} alt = "Dislike"></button>
+                            <button type ="button"><img src = {like} alt = "Like"></button>
+                        </div>
                 </li>
             </ul>
 
